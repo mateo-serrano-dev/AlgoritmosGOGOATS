8a006b18cf74e54ebfcf66cd3ceb956eb1a1b327
Dylann01 <dbonancea@fi.uba.ar>
2026-05-25 14:03:26 -0300

    refactor con models y service + constantes agregadas

--- tp2/database/database.go
+++ tp2/database/database.go
@@ -2,41 +2,39 @@ package database
 
 import (
 	Dict "tdas/diccionario"
-	Logica "tp2/logica"
+	Modelos "tp2/models"
 )
 
 type Database struct {
-	pacientes Dict.Diccionario[string, *Logica.Paciente]
+	pacientes Dict.Diccionario[string, *Modelos.Paciente]
 	//doctores  Dict.DiccionarioOrdenado[string, *Doctor]
-	especialidades Dict.Diccionario[string, *Logica.Especialidad]
+	especialidades Dict.Diccionario[string, *Modelos.Especialidad]
 }
 
-func (db *Database) AgregarPaciente(nombre string, año int, urgencia string) *Logica.Paciente {
-	p := Logica.CrearPaciente(nombre, año, urgencia)
+func (db *Database) ExistePaciente(nombre string) bool {
+	return db.pacientes.Pertenece(nombre)
+}
+
+func (db *Database) ExisteEspecialidad(especialidad string) bool {
+	return db.especialidades.Pertenece(especialidad)
+}
+
+func (db *Database) agregarPaciente(nombre string, año int, urgencia string) *Modelos.Paciente {
+	p := Modelos.CrearPaciente(nombre, año, urgencia)
 	db.pacientes.Guardar(nombre, p)
 	return p
 }
 
-func (db *Database) AgregarEspecialidad(nombre string) *Logica.Especialidad {
-	e := Logica.CrearEspecialidad()
+func (db *Database) agregarEspecialidad(nombre string) *Modelos.Especialidad {
+	e := Modelos.CrearEspecialidad()
 	db.especialidades.Guardar(nombre, e)
 	return e
 }
 
 func (db *Database) EncolarTurno(nombre string, año int, urgencia string, especialidad string) {
-	var p *Logica.Paciente
-	if !db.pacientes.Pertenece(nombre) {
-		p = db.AgregarPaciente(nombre, año, urgencia)
-	} else {
-		p = db.pacientes.Obtener(nombre)
-	}
-
-	var e *Logica.Especialidad
-	if !db.especialidades.Pertenece(especialidad) {
-		e = db.AgregarEspecialidad(especialidad)
-	} else {
-		e = db.especialidades.Obtener(especialidad)
-	}
+	// precondicion: el paciente y la especialidad ya existen en la db
+	p := db.pacientes.Obtener(nombre)
+	e := db.especialidades.Obtener(especialidad)
 
 	e.EnconlarPaciente(p)
 }
