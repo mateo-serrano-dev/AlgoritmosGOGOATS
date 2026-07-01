39d7916a340f2b614538ca5b9b1a174e90187229
Dylann01 <dbonancea@fi.uba.ar>
2026-07-01 02:16:57 -0300

    Comando viaje terminado

--- grafos_util/hierholzer.go
+++ grafos_util/hierholzer.go
@@ -6,29 +6,47 @@ import (
 	TDAPila "tdas/pila"
 )
 
-func hierholzer[T comparable](grafo TDAGrafo.Grafo[T], v T, pila TDAPila.Pila[T], visitados TDADict.Diccionario[Arista[T], bool]) {
+func hierholzer[T comparable](grafo TDAGrafo.GrafoPesado[T], v T, pila TDAPila.Pila[T], visitados TDADict.Diccionario[Arista[T], bool]) {
 	for iter := grafo.IterAdyacentes(v); iter.HayAlgoMas(); iter.Avanzar() {
 		w := iter.VerActual()
-		aristaActual := CrearArista(v, w, 1)
+		peso := grafo.Peso(v, w)
+		aristaActual := CrearArista(v, w, peso)
+		aristaInversa := CrearArista(w, v, peso) // Necesaria para grafos no dirigidos
 		if !visitados.Pertenece(aristaActual) {
 			visitados.Guardar(aristaActual, true)
+			visitados.Guardar(aristaInversa, true)
 			hierholzer(grafo, w, pila, visitados)
 		}
 	}
 	// Previo a apilar visitamos cada vértice adyacente de v
-	// Visitando así cada arista del grafo para cada para vértices
+	// Visitando así cada arista del grafo para cada par vértices
 	pila.Apilar(v)
 }
 
-func Hierholzer[T comparable](grafo TDAGrafo.Grafo[T]) TDAPila.Pila[T] {
+func Hierholzer[T comparable](grafo TDAGrafo.GrafoPesado[T], origen T) TDAPila.Pila[T] {
 	// Precondición: El grafo admite ciclos Eulerianos y no esta vacío
+	// Funciona para grafos pesados o pesados, si es necesario se pasa 1 en el peso de todas las aristas
 	pila := TDAPila.CrearPilaDinamica[T]()
 	visitados := TDADict.CrearHash[Arista[T], bool]()
-	iter := grafo.IterVertices()
-	if !iter.HayAlgoMas() {
-		return pila
-	}
-	pila.Apilar(iter.VerActual())
-	hierholzer(grafo, iter.VerActual(), pila, visitados)
+	hierholzer(grafo, origen, pila, visitados)
 	return pila
+}
+
+func ReconstruirCaminoHierholzer[T comparable](grafo TDAGrafo.GrafoPesado[T], pila TDAPila.Pila[T]) ([]T, float64) {
+	res := make([]T, 0)
+	var pesoTotal float64
+	if pila.EstaVacia() {
+		return res, 0
+	}
+
+	v := pila.Desapilar()
+	res = append(res, v)
+
+	for !pila.EstaVacia() {
+		w := pila.Desapilar()
+		res = append(res, w)
+		pesoTotal += grafo.Peso(v, w)
+		v = w
+	}
+	return res, pesoTotal
 }
